d853d25608e6b930f37173cb691923644139088e
MOOTAZ MEDHAT EZZAT ABDELWAHAB <97257822+MootazMedhatEzzat@users.noreply.github.com>
2023-12-25 16:22:59 +0200

    Update users.php

--- classes/users.php
+++ classes/users.php
@@ -13,7 +13,6 @@ class users extends db_connection {
     private $tel;
     private $account_balance;
     private $user_type;
-    
 
     public function __construct($name, $username, $email, $password, $tel, $user_type) {
         $this->name = $name;
@@ -25,19 +24,18 @@ class users extends db_connection {
     }
     
     public function register() {
-        
         try {
             // Validate user type
             if (!in_array($this->user_type, ['company', 'passenger'])) {
                 throw new Exception("Invalid user type.");
             }
-            
+
             // Check if the user is already registered
-           $auth = new authentication();
-           if ($auth->is_user_registered($this->username, $this->email, $this->tel)) {
-               throw new Exception("User with the same username, email, or tel already exists.");
-           }
-            
+            $auth = new authentication();
+            if ($auth->is_user_registered($this->username, $this->email, $this->tel)) {
+                throw new Exception("User with the same username, email, or tel already exists.");
+            }
+
             parent::connect();  // Connect to the database
 
             // Hash the password
@@ -45,51 +43,54 @@ class users extends db_connection {
 
             // Insert user data into the 'users' table
             $sql = "INSERT INTO users (name, username, email, password, tel, user_type) 
-                   VALUES ('" . $this->name . "', '" . $this->username . "', '" . $this->email . "', '" . $hashedPassword . "', '" . $this->tel . "', '" . $this->user_type . "')";    
-            $query = mysqli_query(parent::get_connect(), $sql);
+                    VALUES (?, ?, ?, ?, ?, ?)";
+            $stmt = mysqli_prepare(parent::get_connect(), $sql);
 
-            // Check if the query was successful
-            if (!$query) {
+            if ($stmt) {
+                // Bind parameters
+                mysqli_stmt_bind_param($stmt, "ssssss", $this->name, $this->username, $this->email, $hashedPassword, $this->tel, $this->user_type);
+
+                // Execute the statement
+                mysqli_stmt_execute($stmt);
+
+                // Get the last inserted ID
+                $lastInsertId = mysqli_insert_id(parent::get_connect());
+
+                // Insert additional data based on user type
+                if ($this->user_type === 'company') {
+                    $sql = "INSERT INTO company (id) VALUES (?)";
+                } elseif ($this->user_type === 'passenger') {
+                    $sql = "INSERT INTO passenger (id) VALUES (?)";
+                }
+
+                // Check if the query was successful
+                if (isset($sql)) {
+                    $stmt = mysqli_prepare(parent::get_connect(), $sql);
+                    if ($stmt) {
+                        // Bind parameters
+                        mysqli_stmt_bind_param($stmt, "i", $lastInsertId);
+    
+                        // Execute the statement
+                        mysqli_stmt_execute($stmt);
+                    }
+                }
+
+                // Registration succeeded
+                echo "Registration successful.";
+                return true;
+            } else {
                 throw new Exception("Failed to insert user data.");
             }
-
-            // Get the last inserted ID
-            $lastInsertId = mysqli_insert_id(parent::get_connect());
-
-            if ($this->user_type === 'company') {
-                // Insert additional data into the 'company' table
-                $sql = "INSERT INTO company (id) VALUES ('" . $lastInsertId . "')";  
-                $query = mysqli_query(parent::get_connect(), $sql);
-
-                // Check if the query was successful
-                if (!$query) {
-                    throw new Exception("Failed to insert company data.");
-                }
-            } elseif ($this->user_type === 'passenger') {
-                // Insert additional data into the 'passenger' table
-                $sql = "INSERT INTO passenger (id) VALUES ('" . $lastInsertId . "')";
-                $query = mysqli_query(parent::get_connect(), $sql);
-
-                // Check if the query was successful
-                if (!$query) {
-                    throw new Exception("Failed to insert passenger data.");
-                }
-            } 
-
-            // Registration succeeded
-            echo "Registration successful.";
-            return true;
-
         } catch (Exception $e) {
             // Handle exceptions (e.g., log the error, display a user-friendly message)
             echo "Registration failed: " . $e->getMessage();
             // Registration failed
             return false;
-
         } finally {
             parent::disconnect();  // Disconnect from the database in all cases
         }
     }
+
 }
 
 ?>
